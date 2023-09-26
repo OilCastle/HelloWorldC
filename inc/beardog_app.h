44fb7162cf2c8cb595ac7d029acf356305f6ea5e
Mr.Beardog <sos880428@gmail.com>
2023-09-27 01:36:49 +0900

    start master

---
+++ inc/beardog_app.h
@@ -0,0 +1,18 @@
+/** @file beardog_app.h
+ *
+ * @brief A description of the module’s purpose.
+ *
+ * @par
+ * COPYRIGHT NOTICE: (c) 2018 Barr Group.  All rights reserved.
+ */
+
+#ifndef BEARDOG_APP_H
+#define BEARDOG_APP_H
+
+
+#define BEARDOG_TEST "Hello World \r\n"
+
+
+#endif /* BEARDOG_APP_H */
+
+/*** end of file ***/
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "beardog_app.h"
+
+
+int main(int, char **)
+{
+    printf(BEARDOG_TEST);
+}
